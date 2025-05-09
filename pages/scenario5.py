b3d0c0e3f5b8e70a60614a9308b8c43cded62253
jackyyim881 <jackyyim881@hotmail.com>
2025-05-09 11:11:00 +0800

    update

--- pages/scenario5.py
+++ pages/scenario5.py
@@ -8,12 +8,14 @@ def main():
     custom_rating_count = 12  # 自定义评分人数 (以K为单位，例如150.5表示150,500人)
     custom_level_confidence = 2
 
+    survey_href = "https://hkbu.questionpro.com/t/AVqakZ587Q"
     # 使用自定义值调用创建页面函数
     create_disney_scenario_page(
         scenario_num=5,
         custom_star_rating=custom_star_rating,
         custom_rating_count=custom_rating_count,
-        custom_level_confidence=custom_level_confidence
+        custom_level_confidence=custom_level_confidence,
+        survey_href=survey_href  # 替换为实际的调查链接
     )
 
 
--- pages/scenario6.py
+++ pages/scenario6.py
@@ -8,12 +8,15 @@ def main():
     custom_rating_count = 12  # 自定义评分人数 (以K为单位，例如150.5表示150,500人)
     custom_level_confidence = 2
 
+    survey_href = "https://hkbu.questionpro.com/t/AVqakZ589r"
+
     # 使用自定义值调用创建页面函数
     create_disney_scenario_page(
         scenario_num=6,
         custom_star_rating=custom_star_rating,
         custom_rating_count=custom_rating_count,
-        custom_level_confidence=custom_level_confidence
+        custom_level_confidence=custom_level_confidence,
+        survey_href=survey_href  # 替换为实际的调查链接
     )
 
 
