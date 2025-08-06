781d2ffcec758a6ef455ef9fc05b3b6d96cca118
kenton0123 <abckentonlam@gmail.com>
2025-08-06 13:15:33 +0800

    Update scenario14.py

--- pages/scenario14.py
+++ pages/scenario14.py
@@ -1,14 +1,20 @@
 # disney_scenario2.py
 import streamlit as st
 from config.disney_scenario_template import create_disney_scenario_page
+from config.utils import capture_and_modify_state
 
 
 def main():
+    capture_and_modify_state(display=False)
+    state = st.session_state.get('state', '')
+    
+    survey_base = "https://surveys.lifepointspanel.com/survey/apac/ISE4911"  # 自定义调查链接
+    survey_href = f"{survey_base}?state={state}"
+    
     custom_star_rating = 4.5  # 自定义星级评分值 (0.0-5.0)
     custom_rating_count = 12  # 自定义评分人数 (以K为单位，例如150.5表示150,500人)
     custom_level_confidence = 2
-    survey_href = "https://hkbu.questionpro.com/t/AVqakZ59Ab"  # 自定义调查链接
-    # 使用自定义值调用创建页面函数
+
     create_disney_scenario_page(
         scenario_num=14,
         custom_star_rating=custom_star_rating,
